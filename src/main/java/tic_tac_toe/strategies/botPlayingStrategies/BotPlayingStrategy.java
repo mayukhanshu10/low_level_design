393c70a2ef3d8e440e118bb0da74e4ea28bc2d23
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2024-04-29 14:58:30 +0530

    Added comment in BotPlayingStrategy

--- src/main/java/tic_tac_toe/strategies/botPlayingStrategies/BotPlayingStrategy.java
+++ src/main/java/tic_tac_toe/strategies/botPlayingStrategies/BotPlayingStrategy.java
@@ -5,6 +5,10 @@ import tic_tac_toe.models.Cell;
 
 public interface BotPlayingStrategy {
 
+    //Here makeMove function returns cell means this method suggest Game class on which cell should bot move.
+    //It's the work of GAME class now to Validate if cell comming from BotPlayingStrategy is valid to move by bot or not.
+    //Valid means if its free or not, Isnt the cell is blocked, etc
+
     Cell makeMove(Board board);
 
 }

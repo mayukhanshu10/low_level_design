6446cb57266354181c2833ba024387319a34b4f0
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2024-05-13 09:48:28 +0530

    Added code and comments in Practical Factory DP

--- src/main/java/design_patterns/CreationalDP/FactoryDP/PracticalDP/Client.java
+++ src/main/java/design_patterns/CreationalDP/FactoryDP/PracticalDP/Client.java
@@ -2,7 +2,16 @@ package design_patterns.CreationalDP.FactoryDP.PracticalDP;
 
 public class Client {
     public static void main(String[] args) {
+
+        /* Calling EmployeeFactory-->getEmployee() which creates object based on Enum type passed by us and returns it.
+           Since WebDeveloper, KotlinDeveloper, etc all are subclasses of Employee Interface type, hence at runn time,
+           Employee e will store the Object of that developer whose type we passed in parameter.
+        */
+
         Employee e = EmployeeFactory.getEmployee(EmployeeType.WEBDEVELOPER);
         System.out.println(e.salary());
+
+        Employee e2 = EmployeeFactory.getEmployee(EmployeeType.KOTLINDEVELOPER);
+        System.out.println(e2.salary());
     }
 }
--- src/main/java/design_patterns/CreationalDP/FactoryDP/PracticalDP/EmployeeFactory.java
+++ src/main/java/design_patterns/CreationalDP/FactoryDP/PracticalDP/EmployeeFactory.java
@@ -2,6 +2,10 @@ package design_patterns.CreationalDP.FactoryDP.PracticalDP;
 
 public class EmployeeFactory {
 
+    /*
+        This is factory class of Employee type, responsible for creating Object as per Enum input comming.
+     */
+
     public static Employee getEmployee(EmployeeType et) {
 
         if(et.equals(EmployeeType.WEBDEVELOPER)){

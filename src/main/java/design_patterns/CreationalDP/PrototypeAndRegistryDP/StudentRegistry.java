b79211c3a845bf32ba2f93b1649f06999d06327a
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2024-05-20 12:20:15 +0530

    Added comments in Prototype and Registry classes.

--- src/main/java/design_patterns/CreationalDP/PrototypeAndRegistryDP/StudentRegistry.java
+++ src/main/java/design_patterns/CreationalDP/PrototypeAndRegistryDP/StudentRegistry.java
@@ -4,6 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StudentRegistry {
+
+    /*
+         This is student registry class therefore its map is storing Student object in its Value.
+         If it would have been general Registry Class ( like Spring Container), the map would have been defined as
+         Map<String, Object> hm = new HashMap<>();
+         THis way we make it more generalised registry class which can store any object prototype.
+     */
+
     Map<String, Student> map = new HashMap<>();
 
     void register(String key, Student student) {
